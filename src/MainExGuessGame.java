25f64096d1167f74c0a5af61d083c1936efe0380
bhavintandel <bhautandel@yahoo.com.com>
2016-03-10 09:35:44 +0000

    fixed bug with range accepted from user

--- src/MainExGuessGame.java
+++ src/MainExGuessGame.java
@@ -9,12 +9,12 @@ import java.util.*;
 
 class GuessGame{
 	public static int countSteps(int maxRange){
-		return (int)(Math.log(maxRange)/Math.log(2)+1e-10);
+		return (int)Math.ceil((Math.log(maxRange)/Math.log(2))); //+1e-10
 	}
 	
 	public static String userInput(int midPoint) throws Exception{
 		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-		System.out.println("Is the number larger (y or yes) than " + midPoint);
+		System.out.println("Is the number strictly larger (y or yes) than " + midPoint);
 	
 			String input = (String) br.readLine();
 			System.out.println(input);
@@ -26,12 +26,17 @@ class GuessGame{
 	
 		int midPoint = (int) Math.floor(((minRange + maxRange)/2));
 		int range;
+		Boolean flag = false;
 		do{
 			midPoint = (int) Math.floor(((minRange + maxRange)/2));
 			String input = userInput(midPoint) ;
 			
 			if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")){
 				minRange = midPoint;
+				// to check if program run in 
+				if( (maxRange - minRange) == 1){
+					flag = true;
+				}
 			}
 			else{
 				maxRange = midPoint;
@@ -39,9 +44,9 @@ class GuessGame{
 			
 			steps--;
 			range = maxRange - minRange;
-		}while (steps >=0);
+		}while (steps >0);
 
-		if (range == 1){
+		if (flag){
 			midPoint++;
 		}
 		
@@ -53,29 +58,31 @@ class GuessGame{
 public class MainExGuessGame extends GuessGame {
 	public static void main(String[] args){
 		int maxRange = 0;
+		int minRange = 0;
 		Boolean flag = false;
 		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 		
 		try{
 			do{
+				System.out.println("Enter the minimum range : ");
+				minRange = Integer.parseInt(br.readLine());
 				System.out.println("Enter the maximum range : ");
-				flag = false;
 				maxRange = Integer.parseInt(br.readLine());
-				if(maxRange < 1){
+				flag = false;
+				if(maxRange < 1 || minRange < 1 || (maxRange < minRange) ){
 					System.out.println("Maximum range should be non-negative and non-zero");
 					flag = true;
 				}
 			}while(flag);
 			
-				int steps = (countSteps(maxRange));
-				System.out.println("You guessed number : " + searchNum(1,maxRange,steps));
+				int steps = (countSteps((maxRange-minRange)+1));
+				System.out.println("You guessed number was guessed in " + steps + " steps: " + searchNum(minRange,maxRange,steps));
 		}
 		catch (Exception e){
 			e.getMessage();
 			e.printStackTrace();
 		}
-		
-		
+			
 		
 	}
 
